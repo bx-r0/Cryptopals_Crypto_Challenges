6973e461fda279d0cbe4301965302aac2bc3679a
Aidan Fray <afray@hotmail.co.uk>
2018-04-25 13:59:29 +0100

    Changes; - S1:C6 - Going to give it a change around; just saving progress

--- Set_1/Challenge6/Challenge6.py
+++ Set_1/Challenge6/Challenge6.py
@@ -3,9 +3,10 @@ sys.path.insert(0, '../..')
 import Function
 import re
 import string
+import collections
+import codecs
 
 KEY_SIZE_RANGE = range(1, 40)
-
 ALPHABET = string.ascii_uppercase
 KEY_SPACE = string.ascii_uppercase + string.ascii_lowercase
 ENGLISH_LANGUAGE_DISTRIBUTION = [8.2,
@@ -39,78 +40,63 @@ ENGLISH_LANGUAGE_DISTRIBUTION = [8.2,
 def task6():
     DATA = load()
 
-    possible_key_size = 0
-    smallest_hamming_value = None
+    # Ranks the most likely key length
+    key_size_and_hamming = rank_possible_key_length(DATA)
 
-    # Splits data into chunk sizes
-    # And works out the possible key size
-    for key_size in KEY_SIZE_RANGE:
+    for key_pair in key_size_and_hamming:
+        possible_key_size = key_pair[0]
 
-        # BASE64 -> HEX conversion going on below
-        key_chunks = split_into_bytes(DATA, key_size)
+        # Breaks the cipher into key size blocks
+        data_chunks = split_into_bytes(DATA, possible_key_size)
 
-        hamming_dist_normalised = (calculate_hamming_distance(key_chunks[0], key_chunks[1])) / key_size
+        # Transposes all the bytes of the chunks
+        transpose_chunks = transpose_bytes(data_chunks, possible_key_size)
 
-        # TODO: Add a ranking system so the smallest few values are present
-        if smallest_hamming_value is None or hamming_dist_normalised < smallest_hamming_value:
-            smallest_hamming_value = hamming_dist_normalised
-            possible_key_size = key_size
+        disovered_key = []
+        # Loops round each letter as a key
+        for transpose_byte in transpose_chunks:
+            frequency_lists = []
+            for letter in KEY_SPACE:
+                # XORs the key attempt with the transposed byte
+                key_attempt = Function.rm_byte(Function.ASCIIToHex(letter * (len(Function.HexToASCII(transpose_byte)))))
 
-    print("Possible key size: ", possible_key_size)
-    possible_key_size = 29
+                xor = Function.strxor(transpose_byte, key_attempt)
 
-    # Breaks the cipher into key size blocks
-    data_chunks = split_into_bytes(DATA, possible_key_size)
+                # Obtains the frequency of the letters for each key
+                frequency = []
+                for letter2 in ALPHABET:
 
-    # Transposes all the bytes of the chunks
-    transpose_chunks = []
-    for key_pos in range(0, possible_key_size):
-        string = ""
-        for x in data_chunks:
-            # Splits into separate bytes and then grabs the current
-            # position of bytes
-            each_hex_byte = re.findall("..", x)
-            string += each_hex_byte[key_pos]
+                    total_occurrence = 0
 
-        transpose_chunks.append(string)
+                    for char in xor:
+                        if char == letter2:
+                            total_occurrence += 1
 
-    disovered_key = []
-    # Loops round each letter as a key
-    for transpose_byte in transpose_chunks:
-        frequency_lists = []
-        for letter in KEY_SPACE:
-            # XORs the key attempt with the transposed byte
-            key_attempt = Function.rm_byte(Function.ASCIIToHex(letter * (len(Function.HexToASCII(transpose_byte)))))
-            xor = Function.strxor(transpose_byte, key_attempt)
+                    frequency.append(total_occurrence)
+                frequency_lists.append([frequency, letter])
 
-            # Obtains the frequency of the letters for each key
-            frequency = []
-            for letter2 in ALPHABET:
-                total_occurance = 0
-                for char in xor:
-                    if char == letter2:
-                        total_occurance += 1
+            # Works out the best key character
+            current_best_key_char = ''
+            smallest_difference = 100000000
+            frequency_pos = 0
+            for frequency in frequency_lists:
+                frequency_dist = frequency[0]
+                key_trial = frequency[1]
 
-                frequency.append(total_occurance)
-            frequency_lists.append(frequency)
+                difference = compare_frequencies(ENGLISH_LANGUAGE_DISTRIBUTION, frequency_dist)
 
-        # Works out the best key character
-        current_best_key_char = ''
-        smallest_difference = 100000000
-        frequency_pos = 0
-        for letter3 in ALPHABET:
-            difference = compare_frequencies(ENGLISH_LANGUAGE_DISTRIBUTION, frequency_lists[frequency_pos])
+                if float(difference) < float(smallest_difference):
+                    smallest_difference = difference
+                    current_best_key_char = key_trial
 
-            if float(difference) < float(smallest_difference):
-                smallest_difference = difference
-                current_best_key_char = letter3
+                frequency_pos += 1
+            disovered_key.append(current_best_key_char)
 
-            frequency_pos += 1
-        disovered_key.append(current_best_key_char)
-
-    # PRINTS KEY
-    for x in disovered_key:
-        print(x, end='')
+        # PRINTS KEY
+        print("Key Size:", possible_key_size, " - ", end='')
+        for x in disovered_key:
+            print(x, end='', flush=True)
+        print()
 
 
 def split_into_bytes(string, number):
@@ -177,5 +163,69 @@ def load():
     for x in lines:
         data += x
     return data
+
+
+def rank_possible_key_length(data):
+    key_size_and_hamming = []
+
+    # Splits data into chunk sizes
+    # And works out the possible key size
+    for key_size in KEY_SIZE_RANGE:
+        # BASE64 -> HEX conversion going on below
+        key_chunks = split_into_bytes(data, key_size)
+
+        hamming_dist_normalised = (calculate_hamming_distance(key_chunks[0], key_chunks[1])) / key_size
+        key_size_and_hamming.append([key_size, hamming_dist_normalised])
+        key_size_and_hamming.sort(key=lambda x: x[1])
+
+    return key_size_and_hamming
+
+
+def transpose_bytes(data_chunks, possible_key_size):
+    for key_pos in range(0, possible_key_size):
+        transpose_chunks = []
+        string = ""
+        for x in data_chunks:
+            # Splits into separate bytes and then grabs the current
+            # position of bytes
+            each_hex_byte = re.findall("..", x)
+            string += each_hex_byte[key_pos]
+
+        transpose_chunks.append(string)
+        return transpose_chunks
+
+
+def single_key_crack(input_str):
+    """
+    TODO
+    :param input_str:
+    :return:
+    """
+    # Uses regex to group values into hex pairs
+    decode = re.findall('..', input_str)
+
+    # Finds the most common hex pair
+    most_common = collections.Counter(decode).most_common(1)[0][0]
+
+    # Adds all single character hex combinations for a key
+    common_characters = []
+    for x in range(0, 255):
+        common_characters.append(str(format(x, '02x')))
+
+    for char in common_characters:
+        # The most common XORd with e will give the key
+        k = Function.strxor(most_common, Function.ASCIIToHex(char))
+        k = str(codecs.decode(k, 'utf-8'))
+        print("Key tried: ", k)
+
+        key = k * round(len(input_str) / 2)
+
+        answer = Function.strxor(input_str, key)
+        answer = Function.HexToASCII(answer)
+
+        # Regex to check string contains alphanumeric values and punctuation
+        if re.match('^[A-Za-z _.,!"\'$]*$', answer) is not None:
+            print("### ANSWER FOUND:\'", answer, '\'')
+            print("Key was: \'", k, '\'')
 
 task6()
