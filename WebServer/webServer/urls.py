5af1e9ba7f5c6c150dfbdb6a6827b6562c97ffcf
Aidan Fray <afray@protonmail.com>
2018-12-30 16:53:28 +0000

    Changes; - Challenge 37 started

--- WebServer/webServer/urls.py
+++ WebServer/webServer/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 import webServer.views as views
+import webServer.SRPServer as SRPServer
 
 urlpatterns = [
     path("", views.index, name='index'),
     path('challenge31', views.challenge31, name='challenge31'),
     path('challenge32', views.challenge32, name='challenge32'),
+    path('SRP/login', SRPServer.login, name='SRP.login'),
+    path('SRP/action', SRPServer.action, name='SRP.action')
 ]
